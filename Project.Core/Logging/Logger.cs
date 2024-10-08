d5a8655e3e41d64395121630edf41737279cf7b3
Tigran <tmanukian91@gmail.com>
2024-10-08 19:38:34 +0400

    Added Loggerr and WebDriverManager classes

---
+++ Project.Core/Logging/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Project.Core.Logging
+{
+	public static class Logger
+	{
+			static Logger()
+			{
+				Log.Logger = new LoggerConfiguration()
+					.MinimumLevel.Debug()
+					.WriteTo.Console()
+					.WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+					.WriteTo.File("Logs/errors.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error)
+					.CreateLogger();
+			}
+
+		public static void Information(string message)
+		{
+			Log.Information(message);
+		}
+
+		public static void Warning(string message)
+		{
+			Log.Warning(message);
+		}
+
+		public static void Error(string message)
+		{
+			Log.Error(message);
+		}
+
+		public static void CloseAndFlush()
+		{
+			Log.CloseAndFlush();
+		}
+	}
+}
---
+++ Project.Core/Settings/WebDriverManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Project.Core.Settings
+{
+	public class WebDriverManager
+	{
+        public IWebDriver? Driver { get; set; }
+
+		public IWebDriver InitializeDriver(string browser)
+		{
+			switch (browser.ToLower())
+			{
+				case "chrome":
+					Driver = new ChromeDriver();
+					break;
+				case "firefox":
+					Driver = new FirefoxDriver();
+					break;
+				default:
+					throw new ArgumentException($"Browser {browser} is not supported");
+			}
+
+			Driver.Manage().Window.Maximize();
+			Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			return Driver;
+		}
+
+		public void QuitDriver()
+		{
+			Driver?.Quit();
+		}
+	}
+}
