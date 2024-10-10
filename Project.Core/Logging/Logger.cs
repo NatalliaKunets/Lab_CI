6c63ad5dfbef0f60c62072cbcd5bcaf10504d0d3
Tigran <tmanukian91@gmail.com>
2024-10-10 18:46:33 +0400

    Fixed the Logger class, added loggerConfiguration.json file, browserType enum and browser settings.

--- Project.Core/Logging/Logger.cs
+++ Project.Core/Logging/Logger.cs
@@ -7,31 +7,7 @@ namespace Project.Core.Logging
 		static Logger()
 		{
 			Log.Logger = new LoggerConfiguration()
-				.MinimumLevel.Debug()
-				.WriteTo.Console()
-				.WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
-				.WriteTo.File("Logs/errors.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error)
 				.CreateLogger();
-		}
-
-		public static void Information(string message)
-		{
-			Log.Information(message);
-		}
-
-		public static void Warning(string message)
-		{
-			Log.Warning(message);
-		}
-
-		public static void Error(string message)
-		{
-			Log.Error(message);
-		}
-
-		public static void CloseAndFlush()
-		{
-			Log.CloseAndFlush();
 		}
 	}
 }
---
+++ Project.Core/Settings/BrowserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Core.Enums;
+
+namespace Project.Core.Settings
+{
+    public class BrowserSettings
+	{
+        public BrowserType browserType{ get; set; }
+        public string URL { get; set; }
+    }
+}
