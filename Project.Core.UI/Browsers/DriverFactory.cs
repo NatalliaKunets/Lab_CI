e77cc65b14a5a095de320d5cdcaf33ffce794576
Tigran <tmanukian91@gmail.com>
2024-10-13 15:18:01 +0400

    Semicolon on namespace

--- Project.Core.UI/Browsers/DriverFactory.cs
+++ Project.Core.UI/Browsers/DriverFactory.cs
@@ -10,31 +10,30 @@ using OpenQA.Selenium.Edge;
 using Project.Core.Enums;
 using Project.Core.Settings;
 
-namespace Project.Core.UI.Browsers
-{
-	public static class DriverFactory
-	{
-		public static IWebDriver InitializeDriver(BrowserSettings settings)
-		{
-			IWebDriver driver;
+namespace Project.Core.UI.Browsers;
 
-			switch (settings.browserType)
-			{
-				case BrowserType.Chrome:
-					var chromeOptions = new ChromeOptions();
-					chromeOptions.AddArgument("--headless");
-					chromeOptions.AddArgument("--no-sandbox");
-					chromeOptions.AddArgument("--disable-dev-shm-usage");
-					driver = new ChromeDriver(chromeOptions);
-					break;
-				case BrowserType.Edge:
-					var edgeOptions = new EdgeOptions();
-					driver = new EdgeDriver(edgeOptions);
-					break;
-				default:
-					throw new ArgumentException($"{settings.browserType} is not supported");
-			}
-			return driver;
+public static class DriverFactory
+{
+	public static IWebDriver InitializeDriver(BrowserSettings settings)
+	{
+		IWebDriver driver;
+
+		switch (settings.browserType)
+		{
+			case BrowserType.Chrome:
+				var chromeOptions = new ChromeOptions();
+				chromeOptions.AddArgument("--headless");
+				chromeOptions.AddArgument("--no-sandbox");
+				chromeOptions.AddArgument("--disable-dev-shm-usage");
+				driver = new ChromeDriver(chromeOptions);
+				break;
+			case BrowserType.Edge:
+				var edgeOptions = new EdgeOptions();
+				driver = new EdgeDriver(edgeOptions);
+				break;
+			default:
+				throw new ArgumentException($"{settings.browserType} is not supported");
 		}
+		return driver;
 	}
 }
