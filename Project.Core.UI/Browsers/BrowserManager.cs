4b868c33119ef3030e3b911e3e8a8b2c87aa950e
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-10-13 15:29:15 +0400

    add BrowserManager singleton

---
+++ Project.Core.UI/Browsers/BrowserManager.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using Project.Core.Settings;
+
+namespace Project.Core.UI.Browsers;
+
+public static class BrowserManager
+{
+    private static IWebDriver? driver;
+
+    public static IWebDriver GetBrowser()
+    {
+        if (driver == null)
+        {
+            BrowserSettings browserSettings = ConfigurationManager.GetBrowserSettings();
+            driver = DriverFactory.InitializeDriver(browserSettings.browserType);
+        }
+
+        return driver;
+    }
+
+    public static void CloseBrowser()
+    {
+        driver?.Quit();
+        driver = null;
+    }
+}
