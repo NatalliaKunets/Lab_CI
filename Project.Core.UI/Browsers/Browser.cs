b4a4151b77f98e24dafd83dd15addead658290f1
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-10-15 16:52:04 +0400

    add Browser, IBrowser

---
+++ Project.Core.UI/Browsers/Browser.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using Project.Core.Logging;
+using System.Collections.ObjectModel;
+
+namespace Project.Core.UI.Browsers;
+
+public class Browser : IBrowser
+{
+    private readonly IWebDriver driver;
+
+    public Browser(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public string Url { get => driver.Url; set => driver.Url = value; }
+
+    public string Title => driver.Title;
+
+    public string CurrentWindowHandle => driver.CurrentWindowHandle;
+
+    public ReadOnlyCollection<string> WindowHandles => driver.WindowHandles;
+
+    public IOptions Manage() => driver.Manage();
+
+    public void Navigate(string url) => driver.Navigate().GoToUrl(url);
+
+    public ITargetLocator SwitchTo() => driver.SwitchTo();
+
+    public void Quit() => driver.Quit();
+    
+    public void Close() => driver.Close();
+
+    public IWebElement FindElement(By by)
+    {
+        try
+        {
+            return driver.FindElement(by);
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, $"Failed to find WebElement by {by}");
+            throw;
+        }
+    }
+
+    public void RefreshPage()
+    {
+        driver.Navigate().Refresh();
+    }
+
+    public void WindowMaximize()
+    {
+        driver.Manage().Window.Maximize();
+    }
+
+}
--- Project.Core/Logging/Logger.cs
+++ Project.Core/Logging/Logger.cs
@@ -9,4 +9,10 @@ public static class Logger
 		Log.Logger = new LoggerConfiguration()
 			.CreateLogger();
 	}
+
+	public static void Error(Exception exception, string message)
+    {
+        Log.Error(exception, message);
+    }
+
 }
