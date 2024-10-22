efdb7f81625299a93b5ab8b7feacc95d4a6afe85
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-10-22 15:54:22 +0400

    add AccountPage and AccountPageLocators

---
+++ Project.Core.UI/PageObjects/Pages/AccountPage.cs
@@ -0,0 +1,17 @@
+﻿using Project.Core.Logging;
+using Project.Core.UI.Browsers;
+using Project.Core.UI.PageObjects.Locators;
+
+namespace Project.Core.UI.PageObjects.Pages;
+
+public class AccountPage : BasePage
+{
+    public AccountPage(IBrowser driver) : base(driver) { }
+
+    public override bool IsPageLoaded()
+    {
+        bool isLoaded = WaitForElement(AccountPageLocators.accountMenu) != null;
+        Logger.Information($"Login page loaded: {isLoaded}");
+        return isLoaded;
+    }
+}
