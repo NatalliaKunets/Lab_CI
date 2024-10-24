c5296a384aca237f55686cd5c66d17398070a248
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-10-24 11:41:55 +0400

    add Login Test with Invalid credentials

--- Project.Core.UI/PageObjects/Locators/LoginPageLocators.cs
+++ Project.Core.UI/PageObjects/Locators/LoginPageLocators.cs
@@ -7,6 +7,6 @@ public static class LoginPageLocators
     public static readonly By userNameInputBy = By.CssSelector("input#login-username");
     public static readonly By passwordInputBy = By.CssSelector("input#login-password");
     public static readonly By loginButtonBy = By.CssSelector("button#login-button");
-    public static readonly By errorUserNameElementBy = By.CssSelector("div[data-encore-id='banner'] span[class*='Message']");
-    public static readonly By errorMessageBy = By.CssSelector("div[data-testid='username-error'] p[class*='sc']");
+    public static readonly By errorMessageBy = By.CssSelector("div[data-encore-id='banner'] span[class*='Message']");
+    public static readonly By errorUserNameElementBy = By.CssSelector("div[data-testid='username-error'] p[class*='sc']");
 }
--- Project.Tests.UI/UserAuthentication.cs
+++ Project.Tests.UI/UserAuthentication.cs
@@ -10,7 +10,7 @@ public class UserAuthentication : BaseTest
     [TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "AT_Lab2024")]
     public void LoginWithValidCredentials(string userName, string password)
     {
-        Logger.Information("Entering Test Log In With Valid Credentials");
+        Logger.Information("Entering Test LogIn With Valid Credentials");
 
         MainPage mainPage = new(Driver!);
         LoginPage loginPage = new(Driver!);
@@ -39,8 +39,31 @@ public class UserAuthentication : BaseTest
     [TestCase("random_user", "pwd123")]
     [TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "123")]
     [TestCase("user", "AT_Lab2024")]
-    public void LoginWithInvalidCredentials()
+    public void LoginWithInvalidCredentials(string userName, string password)
     {
+        Logger.Information("Entering Test LogIn With Invalid Credentials");
+
+        MainPage mainPage = new(Driver!);
+        LoginPage loginPage = new(Driver!);
+
+        mainPage.ClickLoginButton();
+
+        if (loginPage.IsPageLoaded())
+        {
+            loginPage.EnterUserName(userName);
+            loginPage.EnterPassword(password);
+            loginPage.ClickLoginButton();
+        }
+        else
+        {
+            var ex = new Exception("Login Page is not loaded");
+            Logger.Error(ex, "Failed to load Login Page");
+            throw ex;
+        }
+
+        Assert.That(string.Equals(loginPage.GetErrorMessage(), "Incorrect username or password.", StringComparison.InvariantCulture));
+
+        Logger.Information("Test Log In With Invalid Credentials executed.");
     }
 
 }
