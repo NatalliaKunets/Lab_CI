044f7fa9ea651d6957bc9e68b3543d5c12fc2a51
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-10-25 16:04:20 +0400

    fix UserAutentication

--- Project.Tests.UI/UserAuthentication.cs
+++ Project.Tests.UI/UserAuthentication.cs
@@ -7,122 +7,111 @@ namespace Project.Tests.UI;
 [TestFixture]
 public class UserAuthentication : BaseTest
 {
-	[TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "AT_Lab2024")]
-	public void LoginWithValidCredentials(string userName, string password)
-	{
-		Logger.Information("Entering Test LogIn With Valid Credentials");
+    [TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "AT_Lab2024")]
+    public void LoginWithValidCredentials(string userName, string password)
+    {
+        Logger.Information("Entering Test LogIn With Valid Credentials");
 
-		MainPage mainPage = new(Driver!);
-		LoginPage loginPage = new(Driver!);
+        MainPage mainPage = new(Driver!);
+        LoginPage loginPage = new(Driver!);
 
-		mainPage.ClickLoginButton();
+        mainPage.ClickLoginButton();
 
-		if (loginPage.IsPageLoaded())
-		{
-			loginPage.EnterUserName(userName);
-			loginPage.EnterPassword(password);
-			loginPage.ClickLoginButton();
-		}
-		else
-		{
-			var ex = new Exception("Login Page is not loaded");
-			Logger.Error(ex, "Failed to load Login Page");
-			throw ex;
-		}
+        if (!loginPage.IsPageLoaded())
+        {
+            Logger.Error("Failed to load Login Page");
+            Assert.Fail("Login Page is not loaded");
+        }
 
-		Assert.That(mainPage.IsLoggedIn());
+        loginPage.EnterUserName(userName);
+        loginPage.EnterPassword(password);
+        loginPage.ClickLoginButton();
 
-		Logger.Information("Test Log In With Valid Credentials executed.");
-	}
+        Assert.That(mainPage.IsLoggedIn(), "The user is not logged in as expected.");
+
+        Logger.Information("Test Log In With Valid Credentials executed.");
+    }
 
 
-	[TestCase("random_user", "pwd123")]
-	[TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "123")]
-	[TestCase("user", "AT_Lab2024")]
-	public void LoginWithInvalidCredentials(string userName, string password)
-	{
-		Logger.Information("Entering Test LogIn With Invalid Credentials");
+    [TestCase("random_user", "pwd123")]
+    [TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "123")]
+    [TestCase("user", "AT_Lab2024")]
+    public void LoginWithInvalidCredentials(string userName, string password)
+    {
+        Logger.Information("Entering Test LogIn With Invalid Credentials");
 
-		MainPage mainPage = new(Driver!);
-		LoginPage loginPage = new(Driver!);
+        MainPage mainPage = new(Driver!);
+        LoginPage loginPage = new(Driver!);
 
-		mainPage.ClickLoginButton();
+        mainPage.ClickLoginButton();
 
-		if (loginPage.IsPageLoaded())
-		{
-			loginPage.EnterUserName(userName);
-			loginPage.EnterPassword(password);
-			loginPage.ClickLoginButton();
-		}
-		else
-		{
-			var ex = new Exception("Login Page is not loaded");
-			Logger.Error(ex, "Failed to load Login Page");
-			throw ex;
-		}
+        if (!loginPage.IsPageLoaded())
+        {
+            Logger.Error("Failed to load Login Page");
+            Assert.Fail("Login Page is not loaded");
+        }
 
-		Assert.That(string.Equals(loginPage.GetErrorMessage(), "Incorrect username or password.", StringComparison.InvariantCulture));
+        loginPage.EnterUserName(userName);
+        loginPage.EnterPassword(password);
+        loginPage.ClickLoginButton();
 
-		Logger.Information("Test Log In With Invalid Credentials executed.");
-	}
+        Assert.That(string.Equals(loginPage.GetErrorMessage(), "Incorrect username or password.", StringComparison.InvariantCulture),
+            "The error message does not match the expected: 'Incorrect username or password.'");
 
-	[TestCase("", "")]
-	public void LoginWithEmptyFields(string userName, string password)
-	{
-		Logger.Information("Entering Test Login with Empty Fields");
+        Logger.Information("Test Log In With Invalid Credentials executed.");
+    }
 
-		MainPage mainPage = new(Driver!);
-		LoginPage loginPage = new(Driver!);
+    [TestCase("", "")]
+    public void LoginWithEmptyFields(string userName, string password)
+    {
+        Logger.Information("Entering Test Login with Empty Fields");
 
-		mainPage.ClickLoginButton();
+        MainPage mainPage = new(Driver!);
+        LoginPage loginPage = new(Driver!);
 
-		if (loginPage.IsPageLoaded())
-		{
-			loginPage.EnterUserName(userName);
-			loginPage.EnterPassword(password);
-			loginPage.ClickLoginButton();
-		}
-		else
-		{
-			var ex = new Exception("Login Page is not loaded");
-			Logger.Error(ex, "Failed to load Login Page");
-			throw ex;
-		}
+        mainPage.ClickLoginButton();
 
-		Assert.That(string.Equals(loginPage.GetErrorMessage(), "Incorrect username or password.", StringComparison.InvariantCulture));
-		Logger.Information("Test Login with empty fields executed");
-		Logger.Information("Test Login with empty fields executed");
-	}
+        if (!loginPage.IsPageLoaded())
+        {
+            Logger.Error("Failed to load Login Page");
+            Assert.Fail("Login Page is not loaded");
+        }
+
+        loginPage.EnterUserName(userName);
+        loginPage.EnterPassword(password);
+        loginPage.ClickLoginButton();
+
+        Assert.That(string.Equals(loginPage.GetErrorMessage(), "Incorrect username or password.", StringComparison.InvariantCulture),
+            "The error message does not match the expected: 'Incorrect username or password.'");
+        Logger.Information("Test Login with empty fields executed");
+    }
 
 
-	[TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "AT_Lab2024")]
-	public void LogOutTest(string userName, string password)
-	{
-		Logger.Information("Starting Logout Test");
+    [TestCase("31cinettxjyfv2um6x3aa2iqkkdi", "AT_Lab2024")]
+    public void LogOutTest(string userName, string password)
+    {
+        Logger.Information("Starting Logout Test");
 
-		MainPage mainPage = new(Driver!);
-		LoginPage loginPage = new(Driver!);
-		mainPage.ClickLoginButton();
+        MainPage mainPage = new(Driver!);
+        LoginPage loginPage = new(Driver!);
+        mainPage.ClickLoginButton();
 
-		if (loginPage.IsPageLoaded())
-		{
-			loginPage.EnterUserName(userName);
-			loginPage.EnterPassword(password);
-			loginPage.ClickLoginButton();
-			mainPage.ClickUserProfileButton();
-			mainPage.ClickLogoutButton();
-		}
-		else
-		{
-			var ex = new Exception("Login Page is not loaded");
-			Logger.Error(ex, "Failed to load Login Page");
-			throw ex;
-		}
+        if (!loginPage.IsPageLoaded())
+        {
+            Logger.Error("Failed to load Login Page");
+            Assert.Fail("Login Page is not loaded");
+        }
 
-		bool isLoggedOut = mainPage.IsLoggedOut();
-		Assert.That(isLoggedOut, Is.True);
-		Logger.Information("Logout Test executed");
+        loginPage.EnterUserName(userName);
+        loginPage.EnterPassword(password);
+        loginPage.ClickLoginButton();
+        mainPage.ClickUserProfileButton();
+        mainPage.ClickLogoutButton();
 
-	}
+        bool isLoggedOut = mainPage.IsLoggedOut();
+        Assert.That(isLoggedOut, Is.True, "The user is not logged out as expected");
+        Logger.Information("Logout Test executed");
+
+    }
 
 }
