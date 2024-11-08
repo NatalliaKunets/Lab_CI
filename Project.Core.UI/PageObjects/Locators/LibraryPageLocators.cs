ea65858080d7c583026785e1e6046a18c64b5756
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-11-08 23:20:49 +0400

    add test add song to playlist

---
+++ Project.Core.UI/PageObjects/Locators/LibraryPageLocators.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+namespace Project.Core.UI.PageObjects.Locators;
+
+public static class LibraryPageLocators
+{
+    public static readonly By LibraryListBy = By.CssSelector("ul[aria-label='Your Library']");
+    public static readonly By LibraryListItemsBy = By.CssSelector("li[role='listitem']");
+    public static readonly By LibraryThreeDotsBtnBy = By.CssSelector(".Button-sc-1dqy6lx-0.dbhFGF");
+    public static readonly By EditDetailsBtnBy = By.XPath("//button[@role='menuitem' and .//span[text()='Edit details']]");
+    public static readonly By PlaylistNameInputBy = By.CssSelector("input[data-testid='playlist-edit-details-name-input']");
+    public static readonly By SavePlaylistNameBy = By.CssSelector("button[data-testid='playlist-edit-details-save-button']");
+
+    public static readonly string PlaylistByNameTemplate = "//span[contains(text(), '{0}')]/ancestor::li";
+}
---
+++ Project.Core.UI/PageObjects/Pages/LibraryPage.cs
@@ -0,0 +1,107 @@
+﻿using OpenQA.Selenium;
+using Project.Core.Logging;
+using Project.Core.UI.Browsers;
+using Project.Core.UI.Elements;
+using Project.Core.UI.PageObjects.Locators;
+
+namespace Project.Core.UI.PageObjects.Pages;
+
+public class LibraryPage : BasePage
+{
+    public LibraryPage(IBrowser driver) : base(driver)
+    {
+        Driver = driver;
+    }
+
+    private IElement LibraryList => Driver.FindElement(LibraryPageLocators.LibraryListBy);
+    private IReadOnlyCollection<IElement> LibraryListItems => Driver.FindElements(LibraryPageLocators.LibraryListItemsBy);
+    private IElement LibraryThreeDotsBtn => Driver.FindElement(LibraryPageLocators.LibraryThreeDotsBtnBy);
+    private IElement PlaylistNameInput => Driver.FindElement(LibraryPageLocators.PlaylistNameInputBy);
+    private IElement SavePlaylistName => Driver.FindElement(LibraryPageLocators.SavePlaylistNameBy);
+
+    public override bool IsPageLoaded()
+    {
+        try
+        {
+            return WaitForElement(LibraryPageLocators.LibraryListBy) != null;
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return false;
+        }
+    }
+
+    public bool IsPlaylistCreated()
+    {
+        if (LibraryList != null && LibraryListItems.Count > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public IElement? FindPlaylistByName(string playlistName)
+    {
+        By PlaylistByNameBy = By.XPath(string.Format(LibraryPageLocators.PlaylistByNameTemplate, playlistName));
+
+        try
+        {
+            return WaitForElement(PlaylistByNameBy);
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return null;
+        }
+    }
+
+    public void ClickPlaylistByName(string playlistName)
+    {
+
+        try
+        {
+            var playlistElement = FindPlaylistByName(playlistName);
+
+            if (playlistElement == null)
+            {
+                Logger.Error($"Library Page: Playlist '{playlistName}' not found.");
+                throw new NoSuchElementException($"Playlist '{playlistName}' not found.");
+            }
+
+            playlistElement.Click();
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, $"Library Page: Failed to click playlist '{playlistName}'.");
+            throw;
+        }
+    }
+
+    public void EditPlaylistDetails()
+    {
+        try
+        {
+            LibraryThreeDotsBtn.Click();
+            WaitForElement(LibraryPageLocators.EditDetailsBtnBy).Click();
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Library Page: Failed to click on Three Dots Button");
+        }
+    }
+
+    public void RenamePlaylist(string name)
+    {
+        try
+        {
+            WaitForElement(LibraryPageLocators.PlaylistNameInputBy);
+            WaitForElement(LibraryPageLocators.SavePlaylistNameBy);
+            PlaylistNameInput.Clear();
+            PlaylistNameInput.SendKeys(name);
+            SavePlaylistName.Click();
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Library Page: Failed to rename the playlist");
+        }
+    }
+}
