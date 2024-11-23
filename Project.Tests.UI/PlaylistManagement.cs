72f5a22377f0764a7c954a505b23e9790df94bcd
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-11-23 16:51:21 +0400

    change test data

--- Project.Tests.UI/PlaylistManagement.cs
+++ Project.Tests.UI/PlaylistManagement.cs
@@ -93,7 +93,7 @@ public class PlaylistManagement : BaseUITest
         Logger.Information($"Successfully retrieved Search Results for '{songName}'.");
     }
 
-    [TestCase("Thinking Out Loud"), Retry(3)]
+    [TestCase("Bohemian Rhapsody"), Retry(3)]
     public void CreateLikedSongsPlaylist_WhenSongIsAdded(string songName)
     {
         Logger.Information("Starting Test Create Liked Songs Playlist When Song Is Added");
