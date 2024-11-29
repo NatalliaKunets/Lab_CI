4230598f7dda5204a52515427d59d6516202c29f
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-11-29 17:18:27 +0400

    create BaseAPITest

---
+++ Project.Tests.API/BaseAPITest.cs
@@ -0,0 +1,21 @@
+﻿using Project.Core.API.Managers;
+using Project.Core.API.Services;
+using Project.Core.Logging;
+using Project.Core.Settings;
+
+namespace Project.Tests.API;
+
+public class BaseAPITest
+{
+    protected readonly ISessionManager sessionManager;
+    protected readonly AuthenticationService authenticationService;
+
+    public BaseAPITest()
+    {
+        Logger.Information("Starting BaseAPITest");
+
+        var apiSettings = ConfigurationManager.GetApiSettings();
+        sessionManager = new SessionManager(apiSettings);
+        authenticationService = new AuthenticationService(sessionManager);
+    }
+}
