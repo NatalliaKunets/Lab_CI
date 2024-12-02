f84f332c0ad29084179ca1217f5d6561673a8a95
Tigran <tmanukian91@gmail.com>
2024-12-02 13:39:30 +0400

    Added UserService,BaseService classes and UserServiceTests class

---
+++ Project.Core.API/Models/UserProfile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Project.Core.API.Models
+{
+	public record UserProfile
+	{
+		public string DisplayName { get; set; }
+		public ExternalUrls ExternalUrls { get; set; }
+		public Followers Followers { get; set; }
+		public string Href { get; set; }
+		public string Id { get; set; }
+		public List<Image> Images { get; set; }
+		public string Type { get; set; }
+		public string Uri { get; set; }
+	}
+
+	public record ExternalUrls
+	{
+		public string Spotify { get; set; }
+	}
+
+	public record Followers
+	{
+		public string Href { get; set; }
+		public int Total { get; set; }
+	}
+
+	public record Image
+	{
+		public string Url { get; set; }
+		public int Height { get; set; }
+		public int Width { get; set; }
+	}
+}
---
+++ Project.Core.API/Services/Helpers/UserServiceHelper.cs
@@ -0,0 +1,31 @@
+﻿using Project.Core.API.Models;
+
+namespace Project.Core.API.Helpers
+{
+	public static class UserServiceHelper
+	{
+		public static string GetDisplayName(UserProfile profile)
+		{
+			return profile?.DisplayName ?? "Unknown";
+		}
+
+		public static string GetSpotifyUrl(UserProfile profile)
+		{
+			return profile?.ExternalUrls?.Spotify ?? "No Spotify URL available";
+		}
+
+		public static int GetFollowersCount(UserProfile profile)
+		{
+			return profile?.Followers?.Total ?? 0;
+		}
+
+		public static string GetProfileImageUrl(UserProfile profile)
+		{
+			if (profile?.Images?.Count > 0)
+			{
+				return profile.Images[0].Url;
+			}
+			return "No image available";
+		}
+	}
+}
