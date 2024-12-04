68e0a75af12fcefc89ede80eabe5b91ec6811f42
natallia_kunets@epam.com <natallia_kunets@epam.com>
2024-12-04 16:31:42 +0400

    create api test get player playback state

--- Project.Core.API/Models/PlaylistResponse.cs
+++ Project.Core.API/Models/PlaylistResponse.cs
@@ -95,82 +95,75 @@ public record Tracks
     public List<PlaylistTrackObject>? Items { get; set; }
 }
 
-public class PlaylistTrackObject
+public record PlaylistTrackObject
 {
-    public string AddedAt { get; set; } 
+    public string AddedAt { get; set; }
 
-    public AddedByUser AddedBy { get; set; } 
+    public AddedByUser AddedBy { get; set; }
 
-    public bool IsLocal { get; set; } 
+    public bool IsLocal { get; set; }
 
-    public TrackInfo Track { get; set; } 
+    public TrackObject Track { get; set; }
+}
 
-    
-    public class AddedByUser
-    {
-        public ExternalUrls ExternalUrls { get; set; } // Known external URLs for the user.
-        public string Href { get; set; } // API endpoint for the user.
-        public string Id { get; set; } // Spotify user ID.
-        public string Type { get; set; } // Type of object, e.g., "user".
-        public string Uri { get; set; } // Spotify URI for the user.
-        public string DisplayName { get; set; } // User's display name.
-    }
+public record AddedByUser
+{
+    public ExternalUrls ExternalUrls { get; set; } // Known external URLs for the user.
+    public string Href { get; set; } // API endpoint for the user.
+    public string Id { get; set; } // Spotify user ID.
+    public string Type { get; set; } // Type of object, e.g., "user".
+    public string Uri { get; set; } // Spotify URI for the user.
+    public string DisplayName { get; set; } // User's display name.
+}
 
-    public class TrackInfo
-    {
-        public Album Album { get; set; } // Album details for the track.
-        public List<SimplifiedArtist> Artists { get; set; } // List of artists who performed the track.
-        public List<string> AvailableMarkets { get; set; } // Markets where the track can be played.
-        public int DiscNumber { get; set; } // Disc number of the album.
-        public int DurationMs { get; set; } // Track length in milliseconds.
-        public bool Explicit { get; set; } // Whether the track has explicit lyrics.
-        public ExternalIds ExternalIds { get; set; } // Known external IDs for the track.
-        public ExternalUrls ExternalUrls { get; set; } // Known external URLs for the track.
-        public string Href { get; set; } // API endpoint providing full details of the track.
-        public string Id { get; set; } // Spotify ID for the track.
-        public bool? IsPlayable { get; set; } // Indicates if the track is playable in the market.
-        public TrackLinkedFrom LinkedFrom { get; set; } // Details about the originally requested track.
-        public Restrictions Restrictions { get; set; } // Content restrictions for the track.
-        public string Name { get; set; } // Name of the track.
-        public int Popularity { get; set; } // Popularity of the track.
-        public string PreviewUrl { get; set; } // Link to a 30-second preview of the track.
-        public int TrackNumber { get; set; } // Track number in the album.
-        public string Type { get; set; } // Object type, e.g., "track".
-        public string Uri { get; set; } // Spotify URI for the track.
-        public bool IsLocal { get; set; } // Indicates if the track is from a local file.
-    }
+public record TrackObject
+{
+    public Album Album { get; set; } // Album details for the track.
+    public List<SimplifiedArtist> Artists { get; set; } // List of artists who performed the track.
+    public List<string> AvailableMarkets { get; set; } // Markets where the track can be played.
+    public int DiscNumber { get; set; } // Disc number of the album.
+    public int DurationMs { get; set; } // Track length in milliseconds.
+    public bool Explicit { get; set; } // Whether the track has explicit lyrics.
+    public ExternalIds ExternalIds { get; set; } // Known external IDs for the track.
+    public ExternalUrls ExternalUrls { get; set; } // Known external URLs for the track.
+    public string Href { get; set; } // API endpoint providing full details of the track.
+    public string Id { get; set; } // Spotify ID for the track.
+    public bool? IsPlayable { get; set; } // Indicates if the track is playable in the market.
+    public TrackLinkedFrom LinkedFrom { get; set; } // Details about the originally requested track.
+    public Restrictions Restrictions { get; set; } // Content restrictions for the track.
+    public string Name { get; set; } // Name of the track.
+    public int Popularity { get; set; } // Popularity of the track.
+    public string PreviewUrl { get; set; } // Link to a 30-second preview of the track.
+    public int TrackNumber { get; set; } // Track number in the album.
+    public string Type { get; set; } // Object type, e.g., "track".
+    public string Uri { get; set; } // Spotify URI for the track.
+    public bool IsLocal { get; set; } // Indicates if the track is from a local file.
+}
 
-    // Helper classes for nested objects
-    public class Album
-    {
-        public string Name { get; set; }
-        public List<SimplifiedArtist> Artists { get; set; }
-        public string Href { get; set; }
-    }
+public record Album
+{
+    public string Name { get; set; }
+    public List<SimplifiedArtist> Artists { get; set; }
+    public string Href { get; set; }
+}
 
-    public class SimplifiedArtist
-    {
-        public string Name { get; set; }
-        public string Href { get; set; }
-    }
+public record SimplifiedArtist
+{
+    public string Name { get; set; }
+    public string Href { get; set; }
+}
 
-    public class ExternalUrls
-    {
-        public string Spotify { get; set; }
-    }
+public record ExternalIds
+{
+    public string Isrc { get; set; }
+}
 
-    public class ExternalIds
-    {
-        public string Isrc { get; set; }
-    }
+public record TrackLinkedFrom
+{
+    public string Href { get; set; }
+}
 
-    public class TrackLinkedFrom
-    {
-        public string Href { get; set; }
-    }
-
-    public class Restrictions
-    {
-        public string Reason { get; set; }
-    }
+public record Restrictions
+{
+    public string Reason { get; set; }
 }
